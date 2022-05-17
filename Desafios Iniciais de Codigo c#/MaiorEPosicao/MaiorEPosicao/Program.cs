c59bc1565fb5271e79010db060b77a3beab6616c
Caio Hangai <kio.cesar@gmail.com>
2022-05-17 15:20:10 +0900

    Primeiros desafios de código C#

---
+++ Desafios Iniciais de Codigo c#/MaiorEPosicao/MaiorEPosicao/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace MaiorEPosicao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int valorEntrada;
+            int maiorNumero = 0;
+            int posicao = 0;
+
+            for (int i = 1; i <= 100; i++)
+            {
+                valorEntrada = int.Parse(Console.ReadLine());
+
+                if (valorEntrada > maiorNumero)
+                {
+                    maiorNumero = valorEntrada;
+                    posicao = i;
+                }
+            }
+            Console.WriteLine(maiorNumero);
+            Console.WriteLine(posicao);
+        }
+    }
+}
---
+++ Desafios Iniciais de Codigo c#/PoligonosRegularesSimples/PoligonosRegularesSimples/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace PoligonosRegularesSimples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            uint N, L;
+
+            string[] v = Console.ReadLine().Split(' ');
+            N = uint.Parse(v[0]);
+            L = uint.Parse(v[1]);
+
+            uint P = N * L;
+
+            Console.WriteLine(P);
+        }
+    }
+}
